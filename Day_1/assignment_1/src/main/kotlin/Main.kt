1e3202a5bcba76dff337a876b59b39ef87b0e942
ZLozZ <ironman242k2@gmail.com>
2023-06-24 15:32:15 +0700

    Loo_2

--- Day_1/assignment_1/src/main/kotlin/Main.kt
+++ Day_1/assignment_1/src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var bf_value:Int = 0
+    var cr_value: Int = 0
+    for(i in 10..200)
+    {
+        if((i%7)==0 && (i%5)!=0)
+        {
+            cr_value = i
+            if(bf_value != 0)
+                print(",")
+        }
+        if(cr_value!=bf_value)
+        {
+            bf_value = cr_value
+            print(bf_value)
+        }
+    }
+    println(".")
 }
