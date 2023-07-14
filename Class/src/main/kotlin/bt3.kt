c389440b56faea3910450f538d861f2ee5c941cb
ZLozZ <ironman242k2@gmail.com>
2023-07-14 14:38:02 +0700

    update_14_07_2023_Class

---
+++ Class/src/main/kotlin/bt3.kt
@@ -0,0 +1,14 @@
+/*
+Bài 3: Tạo một funtional interface Drawble với phương thức draw(). Thực hiện vẽ
+các hình khối sau bằng dấu *. Viết chương trình có menu cho phép chọn chức năng
+tương ứng và kiểm tra kết quả:
+1. Tạo đối tượng triển khai interface trên để vẽ hình chữ nhật kích thước width, height.
+2. Tạo đối tượng triển khai internface trên để vẽ tam giác vuông góc trái dưới chiều cao h.
+3. Tạo đối tượng triển khai interface trên để vẽ tam giác vuông góc phải trên chiều cao h.
+4. Tạo đối tượng triển khai interface trên để vẽ hình chữ nhật rỗng kích thước width, height
+5. Tạo đối tượng triển khai interface trên để vẽ tam giác cân chiều cao h
+*/
+
+fun main(args: Array<String>) {
+
+}
