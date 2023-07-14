b56560627336457d6e85eab80fcda94cdf37d2d1
ZLozZ <ironman242k2@gmail.com>
2023-07-14 11:02:42 +0700

    update_14_07_2023_Class

---
+++ Class/src/main/kotlin/bt2.kt
@@ -0,0 +1,7 @@
+/*
+Bài 2: Định nghĩa interface mô tả các hành động sau. Triển khai chi tiết và chạy kiểm tra kết quả.
+1. Kiểm tra n có phải s nguyên tố hay không
+2. Kiểm tra n có phải số đẹp(thuận nghịch) hay không
+3. Kiểm tra số đảo của n có phải số nguyên tố không
+4. Kiểm tra tổng các chữ số của n có nguyên tố không
+ */
