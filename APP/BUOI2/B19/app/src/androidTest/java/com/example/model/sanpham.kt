a250eb74054df5addb1c5906ac73274b872cac9c
ZLozZ <ironman242k2@gmail.com>
2023-09-03 17:01:08 +0700

    03/09/2023

---
+++ APP/BUOI2/B19/app/src/androidTest/java/com/example/model/sanpham.kt
@@ -0,0 +1,23 @@
+package com.example.model
+
+import java.io.Serializable
+
+class SanPham: Serializable{
+    private var picture:Int= 0
+    private var position:String = ""
+
+    fun getPicture():Int{
+        return picture
+    }
+    fun setPicture(picture:Int){
+        this.picture = picture
+    }
+
+    fun getPosition():String{
+        return position
+    }
+    fun setPositione(position:String){
+        this.position = position
+    }
+
+}
